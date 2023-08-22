60dd3a20d7213054c77ebc0c103b8c7d6d8cda8e
pedro-donoso <pedro.donoso.r@usach.cl>
2023-08-22 13:52:31 -0400

    agrego permiso usuario

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,6 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: %i[index show]
 
   # GET /properties or /properties.json
   def index
