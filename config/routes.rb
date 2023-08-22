856709def16f955a65f42d727ed1d4387298716d
pedro-donoso <pedro.donoso.r@usach.cl>
2023-08-22 10:15:42 -0400

    creo rutas amigables

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/terms'
-  get 'pages/privacy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/home',    to: 'pages#home'
+  get '/terms',   to: 'pages#terms'
+  get '/privacy', to: 'pages#privacy'
+  root "pages#home"
 end
