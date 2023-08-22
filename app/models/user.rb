5718eaef5db21cb4b59617ce7703bf94cdde2b80
pedro-donoso <pedro.donoso.r@usach.cl>
2023-08-22 12:55:22 -0400

    genero vistas devise solo para user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+ # Creo método para mostrar usuario por defecto
+  def full_name
+    name.blank? ? email : name
+  end
+
 end
