36d7cbbdae1f76964cb6cfe7098f1de4db91dc97
pedro-donoso <pedro.donoso.r@usach.cl>
2023-08-22 11:05:19 -0400

    Valido modelos de referencia Category y Feature

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ApplicationRecord
+
+    # Validaciones Modelo de Referencia Category
+    validates :name,    presence: true,
+                        uniqueness: true
+    validates :available, inclusion: { in: [true, false] }
+
 end
--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,2 +1,6 @@
 class Feature < ApplicationRecord
+    # Validaciones Modelo de Referencia Feature
+    validates :name,    presence: true,
+                        uniqueness: true
+    validates :available, inclusion: { in: [true, false] }
 end
